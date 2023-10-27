90cc7a3888de8b815d5ccb1a0e67196e2ae41d6e
GitHub Action <action@github.com>
2023-10-27 23:32:34 +0000

    Add generated prototypes

---
+++ prototypes/prototype_README.md_ln14.py
@@ -0,0 +1,24 @@
+```python
+import os
+
+def find_todos(file_path):
+    todos = []
+    with open(file_path, 'r') as file:
+        for line_number, line in enumerate(file_path, 1):
+            if '@TODO' in line:
+                todos.append((line_number, line.strip()))
+    return todos
+
+def main():
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file.endswith('.py'):
+                file_path = os.path.join(root, file)
+                todos = find_todos(file_path)
+                for todo in todos:
+                    print(f'In file {file_path}, @TODO at line {todo[0]}: {todo[1]}')
+
+if __name__ == '__main__':
+    main()
+```
+This script will walk through all directories starting from the current directory, and for each Python file it finds, it will look for lines containing `@TODO`. For each such line, it will print the file path, the line number, and the line itself.
