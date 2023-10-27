9f974f1bb083a3f9e47ae2429853a6cb0a966168
Justin Bennington <43761886+justin-bennington@users.noreply.github.com>
2023-10-27 14:58:55 -0400

    feat: add GitHub workflow to generate prototypes from TODO comments
    A new GitHub workflow file `generate_prototypes_from_TODO.yml` was added to the `.github/workflows` directory. This workflow is triggered on push events to the `main` branch. It consists of a job named `generate_prototypes` that runs on the `ubuntu-latest` environment. The job performs the following steps:
    
    1. Checkout code using the `actions/checkout@v2` action.
    2. Set up Python version 3.x using the `actions/setup-python@v2` action.
    3. Install dependencies by upgrading pip and installing the `requests` package.
    4. Run a Python script `scripts/todo.py` that generates prototypes from TODO comments in the code. The script requires the `OPENAI_API_KEY` secret to be set in the GitHub repository.
    5. The script finds TODO comments in `.ts` files and `README.md` file, sends a POST request to the OpenAI API to generate prototypes, and writes the prototypes to `.py` files in the `prototypes` directory.
    6. Finally, the script generates a `TODO.md` file that lists all the TODO comments found in the code.
    
    The purpose of this workflow is to automate the generation of prototypes from TODO comments in the codebase, allowing developers to quickly generate suggested, expert-level completions for their TODO tasks.
    
    feat: add TODO comments for testing the agent
    
    The `README.md` file was modified to add TODO comments for testing the agent. Three TODO comments were added, each suggesting to test the agent by providing a Python file that has a hello world function. These comments serve as reminders for developers to test the agent's functionality.
    
    new file: `TODO.md`
    
    A new file `TODO.md` was added to the repository. This file will be generated by the `scripts/todo.py` script and will contain a list of all the TODO comments found in the codebase, organized by file and line number.
    
    new file: `run.py`
    
    A new file `run.py` was added to the repository. This file serves as an entry point for running the `scripts/todo.py` script. It imports the `main` function from `scripts/todo.py` and executes it when the file is run directly.
    
    new file: `scripts/todo.py`
    
    A new Python script `scripts/todo.py` was added to the repository. This script is responsible for finding TODO comments in `.ts` files and `README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # somewhere-prototyping-agent
+#@TODO: Test the agent by providing a python file that has a hello world function.
+# somewhere-prototyping-agent
+#@TODO: Test the agent by providing a python file that has a hello world function.
+# somewhere-prototyping-agent
+#@TODO: Test the agent by providing a python file that has a hello world function.
---
+++ scripts/todo.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import os
+
+def find_todos_in_file(file_path):
+    todos = []
+    with open(file_path, 'r') as f:
+        for line_number, line in enumerate(f.readlines(), 1):
+            if '@TODO' in line:
+                # Prepare the payload
+                payload = {
+                    "model": "gpt-4-32k",
+                    "messages": [
+                        {"role": "system", "content": "Generates a suggested, expert-level completion of working python code to solve TODOs. Return all your results in .py files. DO NOT INCLUDE ANYTHING BUT EXECUTABLE PYTHON SCRIPT.\nCode context:\n" + str(f)},
+                        {"role": "user", "content": f"TODO: {line}"}
+                    ],
+                    "temperature": 0
+                }
+                # Send the POST request
+                response = requests.post(
+                    'https://api.openai.com/v1/chat/completions',
+                    headers={
+                        'Authorization': f'Bearer {os.getenv("OPENAI_API_KEY")}',
+                        'Content-Type': 'application/json'
+                    },
+                    data=json.dumps(payload)
+                )
+
+                # Extract the prototype from the response
+                # Extract the prototype from the response
+                prototype = response.json()['choices'][0]['message']['content']
+                # Ensure the /prototypes directory exists
+                os.makedirs('prototypes', exist_ok=True)
+                # Write the prototype to a .py file
+                with open(f'prototypes/prototype_{os.path.basename(f.name)}_ln{line_number}.py', 'w') as prototype_file:
+                    prototype_file.write(prototype)
+
+                todos.append((line_number, line.strip()))
+
+def write_todos_to_md(todos, md_file_path):
+    with open(md_file_path, 'w') as f:
+        for file_path, todo_list in todos.items():
+            f.write(f'## {file_path}\n')
+            for line_number, line in todo_list:
+                f.write(f'- [ ] TODO at line {line_number}: {line}\n')
+            f.write('\n')
+def main():
+        todos = {}
+        for root, dirs, files in os.walk('.'):
+            for file in files:
+                if file.endswith('.ts') or file == 'README.md':
+                    file_path = os.path.join(root, file)
+                    todos_in_file = find_todos_in_file(file_path)
+                    if todos_in_file:
+                        todos[file_path] = todos_in_file
+        write_todos_to_md(todos, 'TODO.md')
+        
+if __name__ == '__main__':
+    main()
